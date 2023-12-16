5d80a2acf066fb7dda5ac4a0a929928149dfcb5a
mvives <mvives@apsl.net>
2023-12-16 23:06:57 +0100

    delete statics and options on prod db

--- src/main/settings.py
+++ src/main/settings.py
@@ -78,6 +78,11 @@ if opts.get("ENVIROMENT", "DEVELOP") == "PROD":
             "USER": opts.get("DATABASE_USER"),
             "PASSWORD": opts.get("DATABASE_PASSWORD"),
             "HOST": opts.get("DATABASE_HOST"),
+            "OPTIONS": {
+                'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
+                'charset': 'utf8mb4',
+                "autocommit": True,
+            }
         }
     }
     WSGI_APPLICATION = "main.wsgi.application"
