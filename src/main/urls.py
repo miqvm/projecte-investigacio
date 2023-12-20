0ede6a9c084632a37e1901c168980e8aeb4de9c2
mvives <mvives@apsl.net>
2023-12-20 19:51:54 +0100

    feat: home template

--- src/main/urls.py
+++ src/main/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main import views as main_views
+
 
 urlpatterns = [
+    path("", main_views.IndexView.as_view(), name="home"),
     path("admin/", admin.site.urls),
 ]
